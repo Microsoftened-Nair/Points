36deb3586964bbfb1e490aa830703fdae28610a8
Microsoftened-Nair <nairmeghraj@gmail.com>
2022-06-19 10:35:08 +0530

    Completed Configuration

--- Point_system/settings.py
+++ Point_system/settings.py
@@ -1,5 +1,3 @@
-import django_heroku
-django_heroku.settings(locals())
 """
 Django settings for Point_system project.
 
@@ -124,3 +122,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import django_heroku
+django_heroku.settings(locals())
